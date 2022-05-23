f9fb93c63f8a1eed4109cc22bce7e4347398e39c
joaquinrapetti <j.rapetti92@gmail.com>
2022-05-23 17:44:37 -0300

    component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { useState } from "react";
+import Counter from "./components/Counter";
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
   console.log("render!");
 
+  // Helper Function
+  const handleClick = () => {
+    setCounter((prevCounter) => {
+      return prevCounter + 1;
+    });
+  };
+
+  const handleClickReset = () => {
+    setCounter(0);
+  };
+
+  const isEven = counter % 2 === 0;
+
   return (
     <div>
       <p>El valor del contador es:</p>
-      <h1>{counter}</h1>
-      <button
+      <Counter number={counter} />
+      {/* <button
         onClick={() =>
           setCounter((prevCounter) => {
             return prevCounter + 1;
           })
-        }
-      >
-        Increment
-      </button>
+        } // Without helper function
+      >Increment</button> */}
+      <p>{isEven ? "Is Even" : "Is Odd"}</p>
+      <button onClick={handleClick}>Increment</button>
+      <button onClick={handleClickReset}>Reset</button>
     </div>
   );
 };
---
+++ src/components/Counter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Counter = ({ number }) => {
+  return (
+    <div>
+      <h1>{number}</h1>
+    </div>
+  );
+};
+
+export default Counter;
