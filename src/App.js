2fb3404386f1e652f53e526a263aecb427d29d17
joaquinrapetti <j.rapetti92@gmail.com>
2022-05-23 17:26:46 -0300

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  console.log("render!");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>El valor del contador es:</p>
+      <h1>{counter}</h1>
+      <button
+        onClick={() =>
+          setCounter((prevCounter) => {
+            return prevCounter + 1;
+          })
+        }
+      >
+        Increment
+      </button>
     </div>
   );
-}
+};
 
 export default App;
